c92e715d147c849b30626f8130159c9c13098671
moongHalwa <moongHalwa@github.com>
2018-01-06 02:43:00 +0530

    emailLogin
    firebase emailPass login working.

---
+++ src/components/login/EmailPass.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { View, Text, TextInput, Alert } from 'react-native';
+import { connect } from 'react-redux';
+import { Actions } from 'react-native-router-flux';
+import firebase from 'firebase';
+import Button from '../common/Button';
+import * as actions from '../../actions';
+import fbAccess from '../FirebaseConfig';
+
+class EmailPass extends Component {
+  constructor() {
+    super();
+    this.state = { email: '', password: '', error: '' };
+  }
+  handleEmail = (text) => {
+    this.setState({ email: text });
+  }
+  handlePassword = (text) => {
+    this.setState({ password: text });
+    console.log(text);
+  }
+  login = (email, pass) => {
+    firebase.auth().signInWithEmailAndPassword(email, pass)
+    .then(() => {
+      //after sign in
+      Actions.lobby();
+    })
+    .catch(() => {
+      firebase.auth().createUserWithEmailAndPassword(email, pass)
+      .then(() => {
+        // message of signing up
+        Actions.lobby();
+      })
+      .catch(() => {
+        Alert.alert('something went wrong');
+      });
+    });
+  }
+  render() {
+    return (
+      <View>
+        <View>
+        <TextInput
+             underlineColorAndroid="transparent"
+             placeholder="Email"
+             placeholderTextColor="#9a73ef"
+             autoCapitalize="none"
+             onChangeText = {this.handleEmail} />
+        </View>
+        <View>
+        <TextInput
+             underlineColorAndroid="transparent"
+             placeholder="Password"
+             placeholderTextColor="#9a73ef"
+             autoCapitalize="none"
+             onChangeText={this.handlePassword} />
+        <Button onPress={() => this.login(this.state.email, this.state.password)} >
+        Submit
+        </Button>
+        <Text>
+        {this.state.error};
+        </Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+export default connect(null, actions)(EmailPass);
--- src/components/secondary/SocialConnect.js
+++ src/components/secondary/SocialConnect.js
@@ -1,11 +1,12 @@
 import { Text, View } from 'react-native';
 import React, { Component } from 'react';
+import EmailPass from '../login/EmailPass';
 
 export default class SocialConnect extends Component {
   render() {
     return (
-      <View>
-        <Text>SocialConnect</Text>
+      <View style={{ paddingTop: 20 }}>
+        <EmailPass />
       </View>
     );
   }
