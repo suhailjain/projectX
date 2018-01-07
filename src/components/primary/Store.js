c5979160a28846f35b6c56b4d87ef7988cf3a26d
moongHalwa <moongHalwa@github.com>
2018-01-07 13:03:14 +0530

    ModalViewForStores

--- src/components/primary/Store.js
+++ src/components/primary/Store.js
@@ -1,17 +1,59 @@
-import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import Modal from 'react-native-modal';
 import Button from '../common/Button';
 
-const Store = (props) => {
-  console.log('hi');
-  console.log(props.store);
+class Store extends Component {
+  constructor() {
+    super();
+    this.state = { modalVisible: false };
+  }
+
+  viewModal = () => {
+    console.log('hi');
+    console.log(this.props.store);
+    this.setState({ modalVisible: !this.state.modalVisible });
+  }
+
+
+  render() {
   return (
     <View>
-    <TouchableOpacity>
-    <Text>{props.store.brand}</Text>
-    </TouchableOpacity>
+    <Button onPress={() => {
+      this.viewModal();
+    }}>
+      {this.props.store.brand}
+    </Button>
+    <Modal isVisible={this.state.modalVisible}>
+          <View style={styles.modalView} >
+            <Text>find this store at</Text>
+            <Text>{this.props.store.floor}</Text>
+            <TouchableOpacity onPress={this.viewModal}>
+              <Text>Hide me!</Text>
+            </TouchableOpacity>
+          </View>
+    </Modal>
     </View>
   );
-};
+}
+}
+
+const styles = StyleSheet.create({
+  modalView: {
+    flex: 1,
+    marginRight: 10,
+    marginBottom: 10,
+    marginTop: 10,
+    marginLeft: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
 
 export default Store;
